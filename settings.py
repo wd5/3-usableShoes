e0c7856b78bf0972b120aab7c08c5b4b15d13ba8
Stepa <steeg.xs@gmail.com>
2012-08-01 12:28:10 +1000

    изменил PROJECT_NAME

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 DATABASE_NAME = u'usableShoes'
-PROJECT_NAME = u'practiceshoes'
+PROJECT_NAME = u'practiceshoes' # новое имя
 SITE_NAME = u'Практичная обувь'
 DEFAULT_FROM_EMAIL = u'support@practiceshoes.ru'
 
